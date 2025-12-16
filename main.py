f7777d790cad0a2fe4e24eeb9f8b670ab59305a2
Denis Silaev <kennybeer999@gmail.com>
2025-12-15 23:03:32 -0600

    Добавил вывод автора проекта через переменные окружения

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello from repository!')
+import os
+from dotenv import load_dotenv
+
+def print_author():
+    load_dotenv()
+    author = os.getenv("AUTHOR")
+    print(f"Автор проекта: {author}")
+
+print_author()
